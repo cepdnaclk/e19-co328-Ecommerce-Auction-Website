37a9397777de846a4d0dd4c67f5776f5518bce6d
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-06-17 10:47:35 +0530

    Initialize Web Socket for Notifications

---
+++ backend/src/main/java/com/bidCircle/backend/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.bidCircle.backend.repository;
+import com.bidCircle.backend.entity.Notification;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NotificationRepository{
+
+    
+    
+}
