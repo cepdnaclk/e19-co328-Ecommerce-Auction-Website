ccbb6406b3a9a738d530694a4254d480aec61855
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-06-17 13:31:56 +0530

    Web Socket Backend Complete

--- backend/src/main/java/com/bidCircle/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/bidCircle/backend/config/WebSocketConfig.java
@@ -1,22 +1,23 @@
 package com.bidCircle.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-@Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final NotificationWebSocketHandler webSocketHandler;
+
+    public WebSocketConfig(NotificationWebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/notifications")
+                .setAllowedOrigins("http://localhost:5173");
+
     }
 }
--- backend/src/main/java/com/bidCircle/backend/repository/NotificationRepository.java
+++ backend/src/main/java/com/bidCircle/backend/repository/NotificationRepository.java
@@ -1,14 +1,17 @@
 package com.bidCircle.backend.repository;
+
 import com.bidCircle.backend.entity.Notification;
+import com.bidCircle.backend.*;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class NotificationRepository{
+public interface NotificationRepository extends JpaRepository<Notification, Integer>{
 
-    
-    
-}
+  @Query(value = "SELECT * FROM notification ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    List<Notification> findLatestFive();  
+} 
